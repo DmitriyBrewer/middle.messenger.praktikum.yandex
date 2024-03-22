616ff541fa7173db5128f6ab41bbf5e76ddce694
DmitriyYanenko <paletobay@mail.ru>
2024-03-22 16:25:08 +0300

    spr_2v3

--- src/ui/input/index.ts
+++ src/ui/input/index.ts
@@ -12,9 +12,6 @@ class InputComponent extends Block {
             events: {
                 change: (event) => props.onChange(event.target.value),
                 blur: (event) => props.blur(event.target.value)
-                // console.log(props.blur);
-                // console.log(event.target.value);}
-                // blur: (event) => this.validate(),
             },
             attr: {
                 class: "fake"
@@ -33,39 +30,8 @@ class InputComponent extends Block {
             autocomplete: this.props.autocomplete,
             pattern: this.props.pattern,
             dir: this.props.dir,
-            error:this.props.error
         });
     }
-
-    // validate() {
-    //     const inputElement = this.getContent();
-    //     if (inputElement) {
-    //         console.log(this);
-    //         const value = inputElement.value;
-    //         if (this.props.type === "text") {
-    //             if (value.trim() === "") {
-    //                 this.setProps({ error: "Поле не может быть пустым" });
-    //             } else {
-    //                 this.setProps({ error: "" });
-    //             }
-    //         } else if (this.props.type === "password") {
-    //             if (value.length < 6) {
-    //                 this.setProps({ error: "Пароль должен содержать минимум 6 символов" });
-    //             } else {
-    //                 this.setProps({ error: "" });
-    //             }
-    //         }
-    //     }
-    // }
-
-    // componentDidUpdate(oldProps) {
-    //     console.log("did");
-    //     if (oldProps.error !== this.props.error) {
-    //         console.log("did propd");
-
-    //         this.validate();
-    //     }
-    // }
 }
 
 export default InputComponent;
